e015f10b65e1339e3c2a195122f8e934bf367dc5
YoheiUmezu <ymeyouhei@gmail.com>
2020-06-01 19:03:17 +0900

    configured postgres db.

--- app/core/models.py
+++ app/core/models.py
@@ -9,7 +9,6 @@ class UserManager(BaseUserManager):#このクラスが呼ばれると新しいus
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-
         return user
 
     def create_superuser(self, email, password):
@@ -18,7 +17,6 @@ class UserManager(BaseUserManager):#このクラスが呼ばれると新しいus
         user.is_staff=True
         user.is_superuser=True
         user.save(using=self._db)
-
         return user
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -27,9 +25,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
 
 
