3c7e245fe8f74f8a547d6181462d586ea89549ec
YoheiUmezu <ymeyouhei@gmail.com>
2020-06-05 23:11:20 +0900

    Added image field to recipe model.

--- app/project/views.py
+++ app/project/views.py
@@ -45,10 +45,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return self.queryset.filter(user=self.request.user)
 
     def get_serializer_class(self):
-        """Return appropriate serializer"""
-        if self.action == 'retrieve':#レシピ全体をみる時だけ
+        """Return appropriate serializer class"""
+        if self.action == 'retrieve':
             return serializers.RecipeDetailSerializer
-        
         return self.serializer_class
 
     def perform_create(self, serializer):
